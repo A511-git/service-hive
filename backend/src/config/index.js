25fba851dac02065a6bc66066738b71cd873c97a
Mohammad Atif <mhdatif0511@gmail.com>
2026-01-14 12:29:31 +0530

    Refactor CORS configuration to include FRONTEND_URL and improve origin handling

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -9,3 +9,4 @@ export const ACCESS_SECRET = process.env.ACCESS_TOKEN_SECRET;
 export const REFRESH_SECRET = process.env.REFRESH_TOKEN_SECRET;
 export const ACCESS_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
 export const REFRESH_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
+export const FRONTEND_URL = process.env.FRONTEND_URL;
--- backend/src/express-app.js
+++ backend/src/express-app.js
@@ -3,16 +3,27 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import { ErrorHandler } from "./utils/error-handler.js"
 import { api } from "./api/index.js"
-
+import { FRONTEND_URL } from "./config/index.js"
 
 export const expressApp = (app) => {
 
     app.use(express.json({ limit: '1mb' }));
     app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+
+    const allowedOrigins = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "http://servicehive_frontend:80",
+        FRONTEND_URL
+    ];
     app.use(
         cors({
-            origin: true,        
-            credentials: true,   
+            origin: (origin, cb) => {
+                if (!origin) return cb(null, true);
+                if (allowedOrigins.includes(origin))  return cb(null, true);
+                return cb(new Error(`CORS blocked: ${origin}`), false);
+            },
+            credentials: true,
             exposedHeaders: ["Authorization"],
         })
     );
