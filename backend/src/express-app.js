74ed55fe7f566fbfd3d6dddf4068b0668fc37b76
Mohammad Atif <mhdatif0511@gmail.com>
2026-01-14 13:10:41 +0530

    .

--- backend/src/express-app.js
+++ backend/src/express-app.js
@@ -1,14 +1,13 @@
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import express from "express";
-import { ErrorHandler } from "./utils/error-handler.js"
-import { api } from "./api/index.js"
-import { FRONTEND_URL } from "./config/index.js"
+import { ErrorHandler } from "./utils/error-handler.js";
+import { api } from "./api/index.js";
+import { FRONTEND_URL } from "./config/index.js";
 
 export const expressApp = (app) => {
-
-    app.use(express.json({ limit: '1mb' }));
-    app.use(express.urlencoded({ extended: true, limit: '1mb' }));
+    app.use(express.json({ limit: "1mb" }));
+    app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
     const allowedOrigins = [
         "http://localhost:5173",
@@ -18,21 +17,24 @@ export const expressApp = (app) => {
     ];
     app.use(
         cors({
-            origin: (origin, cb) => {
-                if (!origin) return cb(null, true);
-                if (allowedOrigins.includes(origin))  return cb(null, true);
-                return cb(new Error(`CORS blocked: ${origin}`), false);
+            origin: (origin, callback) => {
+                if (!origin) return callback(null, true);
+                if (allowedOrigins.includes(origin))
+                    return callback(null, true);
+                return callback(new Error("Not allowed by CORS"));
             },
             credentials: true,
+            methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+            allowedHeaders: ["Content-Type", "Authorization"],
             exposedHeaders: ["Authorization"],
         })
     );
+    app.options(/.*/, cors());
+
     app.use(express.static("public"));
     app.use(cookieParser());
 
-    app.use("/api", api())
+    app.use("/api", api());
 
     app.use(ErrorHandler);
-}
-
-
+};
